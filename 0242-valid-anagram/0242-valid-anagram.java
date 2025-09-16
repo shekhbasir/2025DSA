ba5435bce942120a494adc1a1c5089b73ba4551e
shekh basir <shekhbasir9821273349@gmail.com>
2025-09-16 22:48:20 +0530

    Time: 458 ms (5.96%), Space: 44.9 MB (25.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,21 +6,21 @@ class Solution {
 
         ArrayList<Character> st = new ArrayList<>();
 
-        // Add all chars of s into list
+       
         for (int i = 0; i < s.length(); i++) {
             st.add(s.charAt(i));
         }
 
-        // Check chars of t one by one
+       
         for (int j = 0; j < t.length(); j++) {
             char ch1 = t.charAt(j);
             if (st.contains(ch1)) {
-                st.remove(Character.valueOf(ch1)); // ✅ remove by value
+                st.remove(Character.valueOf(ch1)); 
             } else {
-                return false; // agar char nahi mila → not anagram
+                return false;
             }
         }
 
-        return st.isEmpty(); // sab delete ho gaye to true
+        return st.isEmpty(); 
     }
 }
