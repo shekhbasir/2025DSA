6fa013a637e85e6d8d544a7aea305afffec24558
shekh basir <shekhbasir9821273349@gmail.com>
2025-09-16 05:53:46 +0530

    Time: 5 ms (65.33%), Space: 44 MB (76.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        int org=x;
         int rev=0;
-        int num=x;
+        boolean kam=true;
         while(x>0){
-            int h=x%10;
-            rev=(rev*10)+h;
+            int dig=x%10;
+            rev=rev*10+dig;
             x=x/10;
         }
-        if(num==rev){
-            return true;
-        }else{
-            return false ;
+        if(rev!=org){
+            return false;
         }
+        return true;
     }
 }
