7bf1ea574f37b862578800e4f7851db5b2fd90c6
shekh basir <shekhbasir9821273349@gmail.com>
2025-12-14 16:59:37 +0530

    Time: 3 ms (90%), Space: 46.4 MB (22.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,22 @@
-import java.util.ArrayList;
-
-class Solution {  
+class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
+        //now i am going tofinding the code if the is the anagram or not 
+        boolean kam=false;
 
-        ArrayList<Character> st = new ArrayList<>();
+        if(s.length()!=s.length()) return false;
 
-       
-        for (int i = 0; i < s.length(); i++) {
-            st.add(s.charAt(i));
+        char arr1[]=s.toCharArray();
+        char arr2[]=t.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        String sam1=new String(arr1);
+        String sam2=new String(arr2);
+
+        if(sam1.equals(sam2)){
+            kam=true;
         }
-
-       
-        for (int j = 0; j < t.length(); j++) {
-            char ch1 = t.charAt(j);
-            if (st.contains(ch1)) {
-                st.remove(Character.valueOf(ch1)); 
-            } else {
-                return false;
-            }
-        }
-
-        return st.isEmpty(); 
+         return kam;
     }
 }
