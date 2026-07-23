e590a120a94485291bb32a1dd5664902970bf75f
shekh basir <shekhbasir9821273349@gmail.com>
2026-07-23 15:19:23 +0530

    Time: 1 ms (17.62%), Space: 48.4 MB (14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -5,27 +5,26 @@ class Solution {
         int st = 0;
         int last = nums.length - 1;
 
-        // Aapke style mein ek single loop jo dono ends se check karega
+        
         while (st <= last) {
-            // Agar pehla index nahi mila aur start pointer par target hai
+            
             if (nums[st] == target && kam1 == -1) {
                 kam1 = st;
             }
-            // Agar aakhri index nahi mila aur last pointer par target hai
+            
             if (nums[last] == target && kam2 == -1) {
                 kam2 = last;
             }
 
-            // Agar dono mil gaye toh aage loop chalane ki zaroorat nahi, yahin ruk jao
+           
             if (kam1 != -1 && kam2 != -1) {
                 break;
             }
 
-            // Agar st par target nahi mila, toh aage badho
             if (kam1 == -1) {
                 st++;
             }
-            // Agar last par target nahi mila, toh peeche aao
+            
             if (kam2 == -1) {
                 last--;
             }
